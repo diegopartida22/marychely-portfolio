2d1f68878e1e2b92f9a1acb50d00843a9cceb4c0
diegopartida22 <A01641113@tec.mx>
2023-03-26 08:22:51 -0600

    Edit color of services

--- src/components/Services.js
+++ src/components/Services.js
@@ -10,7 +10,7 @@ function Services() {
         <h1 className="services-title">Mis Servicios</h1>
         <div class="row">
           <div class="col-lg-4 col-12">
-            <div class="card">
+            <div id="cardID" class="card">
               <img
                 src={numerology}
                 class="card-img-top"
@@ -33,7 +33,7 @@ function Services() {
             </div>
           </div>
           <div class="col-lg-4 col-12">
-            <div class="card">
+            <div id="cardID" class="card">
               <img
                 src={angel}
                 class="card-img-top"
@@ -56,7 +56,7 @@ function Services() {
             </div>
           </div>
           <div class="col-lg-4 col-12">
-            <div class="card">
+            <div id="cardID" class="card">
               <img
                 src={tarot}
                 class="card-img-top"
