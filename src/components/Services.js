b0ccbc62ab106a8b89e5f1d5f6b7085cd68fd8a7
diegopartida22 <A01641113@tec.mx>
2023-03-27 09:04:07 -0600

    Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,88 +5,67 @@ import numerology from "../images/numerologia.png";
 
 function Services() {
   return (
-    <div className="services-container">
-      <div class="container" className="container">
+    <div className="services">
+      <div class="container">
         <h1 className="services-title">Mis Servicios</h1>
         <div class="row">
           <div class="col-lg-4 col-12">
-<<<<<<< HEAD
             <div className="services-card">
-=======
-            <div class="card">
->>>>>>> 50d1769e3f4312e913c70497952dac924a83228e
-              <img
-                src={numerology}
-                class="card-img-top"
-                alt="numerology"
-                className="services-image"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Numerología</h5>
-                <p class="card-text">
-                  Te ayudaré a descubrir tu camino de vida y a entender los
-                  patrones numéricos que influyen en tu vida.
-                </p>
-                <a
-                  href="https://getbootstrap.com/docs/5.3/customize/color-modes/"
-                  class="btn btn-primary"
-                >
-                  Ver más
-                </a>
+              <div class="card">
+                <img src={numerology} class="card-img-top" alt="numerology" />
+                <div class="card-body">
+                  <h5 class="card-title">Numerología</h5>
+                  <p class="card-text">
+                    Te ayudaré a descubrir tu camino de vida y a entender los
+                    patrones numéricos que influyen en tu vida.
+                  </p>
+                  <a
+                    href="https://getbootstrap.com/docs/5.3/customize/color-modes/"
+                    class="btn btn-primary"
+                  >
+                    Ver más
+                  </a>
+                </div>
               </div>
             </div>
           </div>
           <div class="col-lg-4 col-12">
-<<<<<<< HEAD
             <div className="services-card">
-=======
-            <div class="card">
->>>>>>> 50d1769e3f4312e913c70497952dac924a83228e
-              <img
-                src={angel}
-                class="card-img-top"
-                alt="angeles"
-                className="services-image"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Terapia de ángeles</h5>
-                <p class="card-text">
-                  Te ayudaré a conectarte con tus ángeles y a recibir su guía
-                  amorosa y sabia para superar cualquier desafío o bloqueo.
-                </p>
-                <a
-                  href="https://getbootstrap.com/docs/5.3/customize/color-modes/"
-                  class="btn btn-primary"
-                >
-                  Ver más
-                </a>
+              <div class="card">
+                <img src={angel} class="card-img-top" alt="angeles" />
+                <div class="card-body">
+                  <h5 class="card-title">Terapia de ángeles</h5>
+                  <p class="card-text">
+                    Te ayudaré a conectarte con tus ángeles y a recibir su guía
+                    amorosa y sabia para superar cualquier desafío o bloqueo.
+                  </p>
+                  <a
+                    href="https://getbootstrap.com/docs/5.3/customize/color-modes/"
+                    class="btn btn-primary"
+                  >
+                    Ver más
+                  </a>
+                </div>
               </div>
             </div>
           </div>
           <div class="col-lg-4 col-12">
-<<<<<<< HEAD
             <div className="services-card">
-=======
-            <div class="card">
->>>>>>> 50d1769e3f4312e913c70497952dac924a83228e
-              <img
-                src={tarot}
-                class="card-img-top"
-                alt="tarot"
-                className="services-image"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Lectura de cartas</h5>
-                <p class="card-text">
-                  Te ayudaré a explorar tu futuro y descubrir las opciones y
-                  oportunidades que se presentan ante ti.
-                </p>
-                <a
-                  href="https://getbootstrap.com/docs/5.3/customize/color-modes/"
-                  class="btn btn-primary"
-                >
-                  Ver más
-                </a>
+              <div class="card">
+                <img src={tarot} class="card-img-top" alt="tarot" />
+                <div class="card-body">
+                  <h5 class="card-title">Lectura de cartas</h5>
+                  <p class="card-text">
+                    Te ayudaré a explorar tu futuro y descubrir las opciones y
+                    oportunidades que se presentan ante ti.
+                  </p>
+                  <a
+                    href="https://getbootstrap.com/docs/5.3/customize/color-modes/"
+                    class="btn btn-primary"
+                  >
+                    Ver más
+                  </a>
+                </div>
               </div>
             </div>
           </div>
