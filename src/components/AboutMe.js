1bef0ab050a2afaf69ca104b16836b0442c3ea1c
diegopartida22 <A01641113@tec.mx>
2023-04-07 16:27:42 -0600

    Details

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,10 +10,10 @@ function AboutMe() {
             <div class="col-lg-6 col">
               <h1 className="aboutme-title">¿Quién Soy?</h1>
               <p className="aboutme-description">
-                Como profesional de la numerología y la consultoría en
-                superación personal, mi objetivo es ayudar a las personas a
-                alcanzar su máximo potencial. Creo en el poder de la mente y en
-                la importancia del autoconocimineto para lograr una vida plena y
+                Como profesional de la numerología y consultoría en superación
+                personal, mi objetivo es ayudar a las personas a alcanzar su
+                máximo potencial. Creo en el poder de la mente y en la
+                importancia del autoconocimineto para lograr una vida plena y
                 satisfactoria. Si estás buscando mejorar tu vida y alcanzar tus
                 metas, estoy aquí para ayudarte en el proceso.
               </p>
